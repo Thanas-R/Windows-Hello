179cf47b7a2c905230204d0d1b5bf13bffaaacd4
Thanas R <thanas5.rd@gmail.com>
2026-04-22 20:18:50 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import WinHello from "./Win-Hello";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <WinHello name="dotpmm" />
-  </React.StrictMode>
+  <WinHello name="dotpmm" />
 );
