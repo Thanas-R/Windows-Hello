40a85713f499385b11c9a01e15978c40b6b9c699
Thanas R <thanas5.rd@gmail.com>
2026-04-22 12:35:02 +0000

    Changes
    Co-authored-by: Thanas-R <220031267+Thanas-R@users.noreply.github.com>

---
+++ src/Win-Hello/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./WinHello";
+export type { WinHelloProps } from "./WinHello";
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WinHello from "./Win-Hello";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WinHello name="dotpmm" />
+  </React.StrictMode>
+);
